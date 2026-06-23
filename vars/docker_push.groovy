197d12ca6867bcecb33947a0729c8b3bcca6c58e
OmDeshmukhgit <omdeshmukh2907@gmail.com>
2026-06-23 22:19:50 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,6 @@ def call(String ImgName, String TagName, String DOCKER_USER) {
                                       usernameVariable: 'DOCKER_USER',
                                       passwordVariable: 'DOCKER_PAT')]) {
         sh "echo $DOCKER_PAT | docker login -u $DOCKER_USER --password-stdin"
-        sh "docker tag ${ImgName}:${TagName} $DOCKER_USER/${ImgName}:${TagName}"
         sh "docker push $DOCKER_USER/${ImgName}:${TagName}"
     }
 }
