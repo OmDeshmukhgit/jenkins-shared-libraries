cdaa8592ebf92c5ed27d4f3b1d78a9b9ba68663f
OmDeshmukhgit <omdeshmukh2907@gmail.com>
2026-06-23 21:27:50 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,9 @@
+def call(String ImgName, String TagName, String DOCKER_USER) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhubCred',
+                                      usernameVariable: 'DOCKER_USER',
+                                      passwordVariable: 'DOCKER_PAT')]) {
+        sh "echo $DOCKER_PAT | docker login -u $DOCKER_USER --password-stdin"
+        sh "docker tag ${ImgName}:${TagName} $DOCKER_USER/${ImgName}:${TagName}"
+        sh "docker push $DOCKER_USER/${ImgName}:${TagName}"
+    }
+}
